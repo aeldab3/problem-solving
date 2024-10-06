629eaa3fddfb6283f434b954e6515d9458bdc145
Ahmed Eldab3 <aeldabaa18@outlook.com>
2024-10-07 02:59:40 +0300

    Solve Two_Sum

---
+++ ProblemS_Leetcode/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProblemS_Leetcode;
+using System;
+
+namespace ProblemS_Leetcode
+{
+
+
+
+    class program
+    {
+        static void Main(string[] args)
+
+        {
+            Two_Sum.Run();
+        }
+    }
+}
---
+++ ProblemS_Leetcode/Two_Sum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemS_Leetcode
+{
+    internal class Two_Sum
+    {
+        //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        public static void Run()
+        {
+        Two_Sum twoSum = new Two_Sum();
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        int[] result = twoSum.TwoSum(nums, target);
+        Console.WriteLine($"Two Sum Indices: {result[0]}, {result[1]}");
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < (nums.Count() - 1); i++)
+            {
+                for (int j = i + 1; j < nums.Count(); j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+            return new int[0];
+        }
+    }
+}
