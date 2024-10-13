8cb50b52fe16d095793e83689dfd15e0b0dde5f4
Ahmed Eldab3 <aeldabaa18@outlook.com>
2024-10-13 23:28:46 +0300

    Find the Index of the First Occurrence in a String

---
+++ ProblemS_Leetcode/Find_the_Index_of_the_First_Occurrence_in_a_String.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemS_Leetcode
+{
+    internal class Find_the_Index_of_the_First_Occurrence_in_a_String
+    {
+        //Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+        public static void Run()
+        {
+            string haystack = "AhmedMohammedEldabaa";
+            string needle = "med";
+
+            Solution s = new Solution();
+
+            int result = s.StrStr(haystack, needle);
+
+            Console.WriteLine(result);
+        }
+        public class Solution
+        {
+            public int StrStr(string haystack, string needle)
+            {
+                if (needle.Length == 0) return 0;
+
+                return haystack.IndexOf(needle);
+            }
+        }
+    }
+}
--- ProblemS_Leetcode/Program.cs
+++ ProblemS_Leetcode/Program.cs
@@ -11,4 +11,5 @@ using System;
 //Merge_Two_Sorted_Lists.Run();
 //Remove_Duplicates_from_Sorted_Array.Run();
 //Remove_Duplicates_from_Sorted_Array_II.Run();
-Remove_Element.Run();
+//Remove_Element.Run();
+Find_the_Index_of_the_First_Occurrence_in_a_String.Run();
