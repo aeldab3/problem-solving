d2a75e90e73a3c9510085c8ff05111b1cbba2c75
Ahmed Eldab3 <aeldabaa18@outlook.com>
2024-10-15 09:18:13 +0300

    Search Insert Position

--- ProblemS_Leetcode/Program.cs
+++ ProblemS_Leetcode/Program.cs
@@ -12,4 +12,5 @@ using System;
 //Remove_Duplicates_from_Sorted_Array.Run();
 //Remove_Duplicates_from_Sorted_Array_II.Run();
 //Remove_Element.Run();
-Find_the_Index_of_the_First_Occurrence_in_a_String.Run();
+//Find_the_Index_of_the_First_Occurrence_in_a_String.Run();
+Search_Insert_Position.Run();
---
+++ ProblemS_Leetcode/Search_Insert_Position.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemS_Leetcode
+{
+    internal class Search_Insert_Position
+    {
+        //Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+        public static void Run()
+        {
+            int[] nums1 = [1, 3, 5, 6]; 
+            int target = 5;
+
+            Solution s = new Solution();
+            int targeIndex = s.SearchInsert(nums1, target);
+            Console.WriteLine(targeIndex);
+        }
+        public class Solution
+        {
+            public int SearchInsert(int[] nums, int target)
+            {
+                int left = 0;
+                int right = nums.Length - 1;
+
+                while (left <= right)
+                {
+                    int mid = left + (right - left) / 2;
+
+                    if (nums[mid] == target)
+                    {
+                        return mid;
+                    }
+
+                    else if (nums[mid] > target)
+                    {
+                        right = mid - 1;
+                    }
+
+                    else if (nums[mid] < target)
+                    {
+                        left = mid + 1;
+                    }
+                }
+                return left;
+            }
+        }
+    }
+}
